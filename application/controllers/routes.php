28597b1cd57226744e2ad38d8299a1f6b923a185
Dean Chen <dean.chen@duke.edu>
2011-02-28 23:01:58 -0500

    Clean up spacing/formatting in routes.php

--- application/controllers/routes.php
+++ application/controllers/routes.php
@@ -10,23 +10,18 @@ class Routes extends CI_Controller {
 
 	function index()
 	{
-		
-		
 		$all_routes = $this->routes_model->get_routes();
 		$routes_data = array();
 		foreach ($all_routes as $route) {
 			$routes_data[] = $route->name;
 		}
 		
-		
 		$data = array(
 			'data' => $routes_data,
 			'callback' => $this->input->get('callback', NULL),
 		);
 		
-		
 		if ($data['callback']) $this->load->view('jsonp', $data);
 		else $this->load->view('json', $data);
 	}
-
 }
